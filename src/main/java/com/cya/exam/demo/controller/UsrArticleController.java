039bf601d86fbe186677ae3d2e731e1ffd741398
choiyoungae <choiaeyoung1001@gmail.com>
2022-09-26 16:49:09 +0900

    게시글 리스트, 테스트데이터, 삭제 기능 구현

--- src/main/java/com/cya/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/cya/exam/demo/controller/UsrArticleController.java
@@ -18,6 +18,18 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for(int i=1; i<=10; i++) {
+			int id = ++lastArticleId;
+			String title = "제목"+id;
+			String body = "내용"+id;
+			
+			articles.add(new Article(id, title, body));
+		}
 	}
 
 	@RequestMapping("/usr/article/doAdd")
@@ -30,5 +42,31 @@ public class UsrArticleController {
 		lastArticleId = id;
 
 		return article;
+	}
+
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+
+		return articles;
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article target = null;
+		
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				target = article;
+			}
+		}
+
+		if(target == null) {
+			return String.format("%d번 글은 존재하지 않습니다.", id);
+		} else {
+			articles.remove(target);
+			return String.format("%d번 글이 삭제되었습니다.", id);
+		}
 	}
 }
