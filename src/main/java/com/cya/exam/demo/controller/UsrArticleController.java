1f49aa2007bafba8d59a28dbb4fbd56d7f58ffdb
choiyoungae <choiaeyoung1001@gmail.com>
2022-10-05 16:59:53 +0900

    ResultData에 data1Name 추가

--- src/main/java/com/cya/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/cya/exam/demo/controller/UsrArticleController.java
@@ -49,7 +49,7 @@ public class UsrArticleController {
 		int id = (int) writeArticleRd.getData1();
 		Article article = articleService.getArticle(id);
 		
-		return ResultData.newData(writeArticleRd, article);
+		return ResultData.newData(writeArticleRd, "article", article);
 	}
 
 	@RequestMapping("/usr/article/getArticles")
@@ -57,7 +57,7 @@ public class UsrArticleController {
 	public ResultData<List<Article>> getArticles() {
 		List<Article> articles = articleService.getArticles();
 
-		return ResultData.from("S-3", "게시글 리스트", articles);
+		return ResultData.from("S-3", "게시글 리스트", "articles", articles);
 	}
 
 	@RequestMapping("/usr/article/getArticle")
@@ -69,7 +69,7 @@ public class UsrArticleController {
 			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다.", id));
 		}
 		
-		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), "article", article);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
@@ -91,7 +91,7 @@ public class UsrArticleController {
 		Article article = articleService.getArticle(id);
 
 		if(article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다.", id), id);
+			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다.", id), "id", id);
 		}
 		
 		if(article.getMemberId() != loginedMemberId) {
@@ -99,7 +99,7 @@ public class UsrArticleController {
 		}
 		
 		articleService.deleteArticle(id);
-		return ResultData.from("S-1", Ut.f("%d번 게시물이 삭제되었습니다.", id), id);
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 삭제되었습니다.", id), "id", id);
 	}
 
 	@RequestMapping("/usr/article/doModify")
@@ -121,7 +121,7 @@ public class UsrArticleController {
 		Article article = articleService.getArticle(id);
 
 		if(article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다.", id), id);
+			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다.", id), "id", id);
 		}
 		
 		ResultData actorCanModifyRd = articleService.actorCanModify(loginedMemberId, article);
