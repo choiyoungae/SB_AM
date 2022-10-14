f55b435ebac2cfb6402829469ecc7499fab1d1e0
choiyoungae <choiaeyoung1001@gmail.com>
2022-10-14 18:37:24 +0900

    멀티게시판 도입, board 테이블 생성

--- db.sql
+++ db.sql
@@ -28,6 +28,42 @@ CREATE TABLE `member`(
     delDate DATETIME COMMENT '탈퇴날짜'
 );
 
+# 게시판 테이블 작성
+CREATE TABLE board (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice(공지사항), free1(자유게시판1), free2(자유게시판2), ...',
+    `name` CHAR(50) NOT NULL UNIQUE COMMENT '게시판 이름',
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제여부 (0=삭제 전, 1=삭제 후)',
+    delDate DATETIME COMMENT '삭제날짜'
+)
+
+# 기본 게시판 생성
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'notice',
+`name` = '공지사항';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'free',
+`name` = '자유게시판';
+
+# 게시물 테이블에 boardId 컬럼 추가
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER `memberId`;
+
+# 1,2 번 게시물을 공지사항 게시물로 수정
+UPDATE article
+SET boardId = 1
+WHERE id IN (1,2);
+
+# 3,4 번 게시물을 자유게시판 게시물로 수정
+UPDATE article
+SET boardId = 2
+WHERE id IN (3,4);
 
 # 게시글 테스트 데이터 생성
 
@@ -99,3 +135,4 @@ WHERE memberId = 0;
 
 SELECT * FROM `member`;
 SELECT * FROM article ORDER BY id DESC;
+SELECT * FROM board;
